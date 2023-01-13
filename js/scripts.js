650fb3d33731f86b2b3815601e7f93f20ca19c78
user.name <raquellbezerraa20@gmail.com>
2023-01-13 01:36:38 -0400

    iniciei JS

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+let quantidadeDeCartas;
+
+function adicionaAsCastasAoJogo(){
+
+    //Loop que vai repetir a quantidade de cartas dadas acima;
+    let = quantidadeDeCartas = 0;
+    while ( quantidadeDeCartas < 4 || quantidadeDeCartas > 14 || quantidadeDeCartas % 2 !== 0) {
+        quantidadeDeCartas = prompt ("Escolha o número de cartas que você quer jogar. Sendo eles números pares (entre 4 e 14).");
+    }
+
+}
+
+adicionaAsCastasAoJogo();
+
+
